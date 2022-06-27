ee5ac152d00e548f61c7673640695389e1d1ef45
Ramil Saavedra <ramilsmcl@gmail.com>
2022-06-27 14:15:25 +0800

    apply prettier on editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,32 +1,60 @@
-import React from 'react';
-import Editor from '@monaco-editor/react';
-import { OnChange } from '@monaco-editor/react';
+import React, { useRef, useCallback } from 'react';
+import Editor, { OnMount, EditorProps } from '@monaco-editor/react';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
-interface CodeEditorProps {
-  onChange: OnChange;
-  defaultValue: string;
-}
+const CodeEditor: React.FC<EditorProps> = ({ onChange, defaultValue }) => {
+  const editorref = useRef<monaco.editor.IStandaloneCodeEditor>();
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, defaultValue }) => {
+  const onMount: OnMount = (editor, monaco) => {
+    editorref.current = editor;
+  };
+
+  const onClickFormat = () => {
+    if (!editorref.current) {
+      return;
+    }
+
+    const unformatted = editorref.current.getValue();
+
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+
+    editorref.current.setValue(formatted);
+  };
+
   return (
-    <Editor
-      options={{
-        fontSize: 16,
-        automaticLayout: true,
-        lineNumbersMinChars: 3,
-        scrollBeyondLastLine: false,
-        showUnused: false,
-        wordWrap: 'on',
-        minimap: {
-          enabled: false,
-        },
-      }}
-      theme='vs-dark'
-      height='50vh'
-      defaultLanguage='javascript'
-      defaultValue={defaultValue}
-      onChange={onChange}
-    />
+    <div className='editor'>
+      <button className='button-format' onClick={onClickFormat}>
+        Format
+      </button>
+      <Editor
+        options={{
+          fontSize: 16,
+          automaticLayout: true,
+          lineNumbersMinChars: 3,
+          scrollBeyondLastLine: false,
+          showUnused: false,
+          wordWrap: 'on',
+          minimap: {
+            enabled: false,
+          },
+        }}
+        theme='vs-dark'
+        height='50vh'
+        defaultLanguage='javascript'
+        defaultValue={defaultValue}
+        onChange={onChange}
+        onMount={onMount}
+      />
+    </div>
   );
 };
+
 export default CodeEditor;
